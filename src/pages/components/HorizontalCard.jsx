c933d12a1d394ca11c70783f028c40869605b32b
Your Name <you@example.com>
2024-03-11 17:00:45 +0000

    chnaged cards on home to a component

---
+++ src/pages/components/HorizontalCard.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const HorizontalCard = ({ icon, heading }) => {
+  return (
+    <>
+      <div className="home_card_body flex gap-3 items-center bg-white min-w-[250px] max-w-[350px] rounded-lg shadow-lg text-[26px] cursor-pointer hover:translate-y-1 transition-transform">
+        <div className="icon_container bg-[#1C3925] p-2">
+          <p className="icon">{icon}</p>
+        </div>
+        <div className="text_container">{heading}</div>
+      </div>
+    </>
+  );
+};
+
+export default HorizontalCard;
--- src/pages/parent/ParentHome.jsx
+++ src/pages/parent/ParentHome.jsx
@@ -4,6 +4,7 @@ import { BiPlusMedical } from "react-icons/bi";
 import { GiMedicines } from "react-icons/gi";
 import { BiSolidInjection } from "react-icons/bi";
 import { FiMessageSquare } from "react-icons/fi";
+import HorizontalCard from "../components/HorizontalCard";
 
 const ParentHome = () => {
   return (
@@ -20,30 +21,22 @@ const ParentHome = () => {
         </p>
 
         <div className="home_card_container bg-gray-100 p-3 my-5 grid grid-cols-2 gap-5">
-          <div className="home_card_body flex gap-3 items-center bg-white min-w-[250px] max-w-[350px] rounded-lg shadow-lg text-[26px] cursor-pointer hover:translate-y-1 transition-transform">
-            <div className="icon_container bg-[#1C3925] p-2">
-              <BiPlusMedical size={50} color="white" />
-            </div>
-            <div className="text_container">Departments</div>
-          </div>
-          <div className="home_card_body flex gap-3 items-center bg-white min-w-[250px] max-w-[350px] rounded-lg shadow-lg text-[26px] cursor-pointer hover:translate-y-1 transition-transform">
-            <div className="icon_container bg-[#1C3925] p-2">
-              <GiMedicines size={50} color="white" />
-            </div>
-            <div className="text_container">Medication</div>
-          </div>
-          <div className="home_card_body flex gap-3 items-center bg-white min-w-[250px] max-w-[350px] rounded-lg shadow-lg text-[26px] cursor-pointer hover:translate-y-1 transition-transform">
-            <div className="icon_container bg-[#1C3925] p-2">
-              <BiSolidInjection size={50} color="white" />
-            </div>
-            <div className="text_container">Procedures</div>
-          </div>
-          <div className="home_card_body flex gap-3 items-center bg-white min-w-[250px] max-w-[350px] rounded-lg shadow-lg text-[26px] cursor-pointer hover:translate-y-1 transition-transform">
-            <div className="icon_container bg-[#1C3925] p-2">
-              <FiMessageSquare size={50} color="white" />
-            </div>
-            <div className="text_container">Messages</div>
-          </div>
+          <HorizontalCard
+            icon={<BiPlusMedical size={50} color="white" />}
+            heading="Departments"
+          />
+          <HorizontalCard
+            icon={<GiMedicines size={50} color="white" />}
+            heading="Medicine"
+          />
+          <HorizontalCard
+            icon={<BiSolidInjection size={50} color="white" />}
+            heading="Procedures"
+          />
+          <HorizontalCard
+            icon={<FiMessageSquare size={50} color="white" />}
+            heading="Messages"
+          />
         </div>
       </div>
     </>
