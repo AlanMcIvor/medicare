3d9a45d3edec04e8d1e51a0d7c75a6ba645b76ac
Your Name <you@example.com>
2024-03-20 15:38:56 +0000

    login

--- src/pages/shared/Login.jsx
+++ src/pages/shared/Login.jsx
@@ -1,14 +1,58 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-  const [email, setEmail] = useState("");
+  // const [email, setEmail] = useState("");
+  const [patientNumber, setPatientNumber] = useState("");
   const [password, setPassword] = useState("");
+  const [parent, setParent] = useState(false);
+  const navigate = useNavigate();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  const handleLogin = async () => {
+    // make a post request to the login endpoint witrh patient number and password
+    try {
+      const response = await axios.post("http://localhost:3000/api/Login", {
+        patientNumber,
+        password,
+      });
 
-    console.log(email, password);
+      //if login is succesful, log a messege and the token recieved from teh server
+      console.log("Login Successful");
+      console.log("Token: ", response.data.token);
+
+      // save token to local storage
+      localStorage.setItem("token", response.data.token);
+      // save patient num to local storage
+      localStorage.setItem("patientNum", response.data.patientNumber);
+      // save parent state to local storage
+      if (parent) {
+        localStorage.setItem("parent", true);
+      } else {
+        localStorage.removeItem("parent");
+      }
+
+      // if user is parent, redirect to the parent dashboard
+      if (parent) {
+        navigate("/ParentHome");
+      } else {
+        navigate("/PatientHome");
+      }
+    } catch (error) {
+      // if login fails, log the error messaage
+      console.error("Login Failed:", error.response.data.error);
+    }
   };
+
+  // load parent state from local storage
+  useEffect(() => {
+    // get parent state from local storage
+    const storedParent = localStorage.getItem("parent");
+    // if parent true set to true
+    if (storedParent) {
+      setParent(true);
+    }
+  }, []);
 
   return (
     <>
@@ -29,11 +73,11 @@ const Login = () => {
       <form className="text-center text-[26px] flex flex-col gap-5 items-center justify-center">
         <input
           className="border rounded-lg shadow-md min-w-[500px] p-2 text-left"
-          type="email"
-          id="email"
-          placeholder="Email"
-          onChange={(e) => setEmail(e.target.value)}
-          value={email}
+          type="string"
+          id="patientNumber"
+          placeholder="Patient Number"
+          onChange={(e) => setPatientNumber(e.target.value)}
+          value={patientNumber}
         />
         <input
           className="border rounded-lg shadow-md min-w-[500px] p-2 text-left"
@@ -46,7 +90,7 @@ const Login = () => {
       </form>
       <div className="btn_container mt-10 text-center">
         <input
-          onClick={handleSubmit}
+          onClick={handleLogin}
           type="submit"
           className="text-2xl bg-[#1C3925] text-white min-w-[250px] px-2 py-3 rounded-full shadow-lg font-body cursor-pointer hover:translate-y-2 transition-transform"
         />
